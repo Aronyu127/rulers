0854d6cf7dea5dea6a16983a958d76c0133d4d4a
aronyu <h0935674938@hotmail.com>
2016-11-15 22:22:55 +0800

    前兩章

---
+++ lib/rulers.rb
@@ -0,0 +1,39 @@
+require "rulers/version"
+require "rulers/array"
+require "rulers/routing"
+require "rulers/dependencies"
+
+module Rulers
+  class Application
+    def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+
+      if env['PATH_INFO'] == '/'
+        env['PATH_INFO'] = '/quotes/a_quote'
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+      rescue
+        text = 'something wrong'
+      end
+        [200, {'Content-Type' => 'text/html'},
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
+    end
+  end
+end
